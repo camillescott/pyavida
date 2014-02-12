4082aba7ffc8fe4f8e0038ae2268106748db26b5
Camille Scott <camille.scott.w@gmail.com>
2014-02-12 01:45:50 -0500

    adde qsub code

--- avidarunlauncher.py
+++ avidarunlauncher.py
@@ -1,5 +1,6 @@
 import jinja2
 import argparse
+import subprocess
 from config import *
 
 def build_scripts(folders, args, jenv):
@@ -12,6 +13,21 @@ def build_scripts(folders, args, jenv):
                 for folder in folders]
     return scripts
 
+def write_scripts(folders, scripts):
+    script_files = []
+    for folder, script in zip(folders, scripts):
+        fn = os.path.join(folder, 'run_avida.pbs')
+        with open(fn, 'w') as outfp:
+            outfp.write(script)
+            script_files.append(fn)
+    return script_files
+
+def qsub_script(script_file):
+    cmd = ['qsub', script_file]
+    p = subprocess.Popen(cmd, stdout=subprocess.PIPE)
+    jid = ''.join(p.stdout)
+    return jid
+
 def main():
     parser = argparse.ArgumentParser()
     parser.add_argument('-i', '--folderlist', dest='folderlist', 
@@ -22,11 +38,23 @@ def main():
         default=BUYIN)
     parser.add_argument('--job-prefix', dest='job_prefix',
         default=JOB_PREFIX)
+    parser.add_argument('--submit', dest='submit', action='store_true')
     args = parser.parse_args()
 
     folders = args.folderlist.read().split('\t')
     
     jenv = Environment(loader=PackageLoader('pyavida', 'templates'))
 
+    print >>sys.stderr, 'building job scripts from templates...'
     scripts = build_scripts(folders, args, jenv)
+    print >>sys.stderr, 'writing scripts to files...'
+    script_files = write_scripts(folders, scripts)
     
+    if args.submit:
+        print >>sys.stderr, 'submitting jobs to torque...'
+        for fn in script_files:
+            jid = qsub_script(fn)
+            print jid
+
+if __name__ == '__main__':
+    main()
