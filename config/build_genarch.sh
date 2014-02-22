a2805fde28661be026bb67b013c7a71cd3584751
Camille Scott <camille.scott.w@gmail.com>
2014-02-22 18:21:46 -0500

    updated run scripts

--- config/build_genarch.sh
+++ config/build_genarch.sh
@@ -11,5 +11,5 @@ odir=$2_$cname
 
 python $py/pyavida/avidasimbuilder.py -f --prefix $odir --outdir /mnt/scratch/tg/w/$odir --avidacfg $cdir/avida.cfg --events $cfg/events.cfg --env $cfg/environment.cfg --analyze $cfg/analyze.cfg --org $cfg/default-heads.org --inst $cfg/instset-heads.cfg -x $3 > $cdir/$odir.workspaces
 
-cat $cdir/$odir.workspaces | python $py/pyavida/avidarunlauncher.py --resources nodes=1:ppn=1,mem=4gb,walltime=24:00:00 --buyin ged-intel11 --run --analyze --submit
+cat $cdir/$odir.workspaces | python $py/pyavida/avidarunlauncher.py --resources nodes=1:ppn=1,mem=4gb,walltime=48:00:00 --buyin ged-intel11 --run --analyze --submit
 
